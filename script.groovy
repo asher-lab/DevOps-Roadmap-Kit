90aba16331c91ca569c671347f0c63aa9f2c95be
Asher <51806917+asher-lab@users.noreply.github.com>
2021-08-27 09:25:24 +0800

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildApp() {
+	echo 'building the application..'
+}
+
+
+def testApp() {
+	echo 'testing the application..'
+}
+
+def deployApp() {
+	echo 'deploying the application..'
+	echo 'deploying the application.. version $(params.VERSION)'
+}
+
+return this
+
